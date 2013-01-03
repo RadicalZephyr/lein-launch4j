eec0ee1b5a83f908ce546ed6b0ebb01e96b7e2dc
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-02 18:46:02 -0800

    Stub out the skeleton of functionality

--- src/leiningen/launch4j.clj
+++ src/leiningen/launch4j.clj
@@ -1,6 +1,26 @@
-(ns leiningen.launch4j)
+(ns leiningen.launch4j
+  (:import net.sf.launch4j.Log
+           net.sf.launch4j.Builder
+           net.sf.launch4j.config.ConfigPersister))
+"So the point of this plugin might be just to handle generating the
+XML for you, and then feeding it to the program with the correct base directory.
+
+Builder can take an optional basedir"
+
+(defn make-xml-file [project])
+
+(defn read-config [config-file]
+  (.. (ConfigPersister/getInstance)
+      (load config-file)))
+
+(defn build-project [bin-dir]
+  (.. (Builder. (Log/getConsoleLog) bin-dir)
+      (build)))
+
+(defn get-launch4j-bin-dir [project])
 
 (defn launch4j
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (read-config (make-xml-file project))
+  (build-project (get-launch4j-bin-dir project)))
