eac7f2f57344d90c62e408ec72d3da85b239373c
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-02 18:56:42 -0800

    Clean up and simplify implementation for 0.1.0

--- src/leiningen/launch4j.clj
+++ src/leiningen/launch4j.clj
@@ -2,12 +2,6 @@
   (:import net.sf.launch4j.Log
            net.sf.launch4j.Builder
            net.sf.launch4j.config.ConfigPersister))
-"So the point of this plugin might be just to handle generating the
-XML for you, and then feeding it to the program with the correct base directory.
-
-Builder can take an optional basedir"
-
-(defn make-xml-file [project])
 
 (defn read-config [config-file]
   (.. (ConfigPersister/getInstance)
@@ -17,10 +11,8 @@ Builder can take an optional basedir"
   (.. (Builder. (Log/getConsoleLog) bin-dir)
       (build)))
 
-(defn get-launch4j-bin-dir [project])
-
 (defn launch4j
   "I don't do a lot."
   [project & args]
-  (read-config (make-xml-file project))
-  (build-project (get-launch4j-bin-dir project)))
+  (read-config (:launch4j-config project))
+  (build-project (:launch4j-install project)))
