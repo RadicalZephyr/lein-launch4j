3eaea9c72233a1a98d75a94fbb47cc9f0e47fb2d
Geoff Shannon <earthlingzephyr@gmail.com>
2013-10-05 09:38:14 -0700

    Fix data representation to be compilable to xml

--- src/leiningen/launch4j.clj
+++ src/leiningen/launch4j.clj
@@ -5,11 +5,10 @@
            net.sf.launch4j.config.ConfigPersister))
 
 (def basic-form
-  [:launch4jConfig {} []])
+  [:launch4jConfig {}])
 
 (def top-level-elements
-  [
-   [:headerType {} ""]
+  [[:headerType {} ""]
    [:outfile {} ""]
    [:jar {} ""]
    [:dontWrapJar {} ""]
@@ -22,51 +21,50 @@
    [:customProcName {} ""]
    [:stayAlive {} ""]
    [:icon {} ""]
-   [:classpath {} [
-                   [:mainClass {} []]
-                   [:cp {} []]
-                   ]]
-   [:singleInstance {} [
-                        [:mutexName {} []]
-                        [:windowTitle {} []]
-                        ]]
-   [:jre {} [
-             [:path {} []]
-             [:minVersion {} []]
-             [:maxVersion {} []]
-             [:jdkPreference {} []]
-             [:initialHeapSize {} []]
-             [:initialHeapPercent {} []]
-             [:maxHeapSize {} []]
-             [:maxHeapPercent {} []]
-             [:opt {} []]
-             ]]
-   [:splash {} [
-                [:file {} []]
-                [:waitForWindow {} []]
-                [:timeout {} []]
-                [:timeoutErr {} []]
-                ]]
-   [:versionInfo {} [
-                     [:fileVersion {} []]
-                     [:txtFileVersion {} []]
-                     [:fileDescription {} []]
-                     [:copyright {} []]
-                     [:productVersion {} []]
-                     [:txtProductVersion {} []]
-                     [:productName {} []]
-                     [:companyName {} []]
-                     [:internalName {} []]
-                     [:originalFilename {} []]
-                     ]]
-   [:messages {} [
-                  [:startupErr {} []]
-                  [:bundledJreErr {} []]
-                  [:jreVersionErr {} []]
-                  [:launcherErr {} []]
-                  ]]
+   [:classpath {}
+    [:mainClass {} ""]
+    [:cp {} ""]]
+   [:singleInstance {}
+    [:mutexName {} ""]
+    [:windowTitle {} ""]]
+   [:jre {}
+    [:path {} ""]
+    [:minVersion {} ""]
+    [:maxVersion {} ""]
+    [:jdkPreference {} ""]
+    [:initialHeapSize {} ""]
+    [:initialHeapPercent {} ""]
+    [:maxHeapSize {} ""]
+    [:maxHeapPercent {} ""]
+    [:opt {} ""]]
+   [:splash {}
+    [:file {} ""]
+    [:waitForWindow {} ""]
+    [:timeout {} ""]
+    [:timeoutErr {} ""]]
+   [:versionInfo {}
+    [:fileVersion {} ""]
+    [:txtFileVersion {} ""]
+    [:fileDescription {} ""]
+    [:copyright {} ""]
+    [:productVersion {} ""]
+    [:txtProductVersion {} ""]
+    [:productName {} ""]
+    [:companyName {} ""]
+    [:internalName {} ""]
+    [:originalFilename {} ""]]
+   [:messages {}
+    [:startupErr {} ""]
+    [:bundledJreErr {} ""]
+    [:jreVersionErr {} ""]
+    [:launcherErr {} ""]]])
 
-   ])
+(defn emit-empty-config []
+  (println
+   (xml/emit-str
+    (xml/sexp-as-element (vec
+                          (concat basic-form
+                                  top-level-elements))))))
 
 (def repeatable-elements
   [:obj
