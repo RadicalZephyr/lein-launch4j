8ca43f2808f673c6a4f3cae8161de927f6879b0d
gus macaulay <gusmacaulay@gmail.com>
2016-01-16 21:06:41 +1100

    Upgraded launch4j version.
    Seems to fix issues with executable not finding java.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-launch4j "0.1.1-SNAPSHOT"
+(defproject lein-launch4j "0.1.2-SNAPSHOT"
   :description "wrap your project with launch4j"
   :url "https://github.com/ezephyr/lein-launch4j"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[launch4j "3.0.2"]])
+  :dependencies [[net.sf.launch4j/launch4j "3.8.0"]])
